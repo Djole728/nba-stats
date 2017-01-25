8939663c27c8a1de37a2b7ae0cf9b19fb1b8775f
Siniša Đorđić <djordjics@lanaco.com>
2017-01-25 16:10:17 +0100

    Dodata navigacija

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,11 @@
 var React = require('react');
+var NavBar = require('./NavBar');
 
 var Main = React.createClass({
   render: function () {
     return (
       <div className='main-container'>
+      <NavBar />
         {this.props.children}
       </div>
     )
